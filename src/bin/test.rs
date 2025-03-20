e5de6435eec6abb430b13fa4e23d7ac59f2f009f
Jonathan Nilsson <jonathan@voysys.se>
2025-03-20 15:23:21 +0100

    Hmmm

---
+++ src/bin/test.rs
@@ -0,0 +1,7 @@
+use steamdeck_input_rs::SteamdeckInput;
+
+fn main() {
+    let steamdeck_input = SteamdeckInput::new();
+
+    loop {}
+}
--- src/lib.rs
+++ src/lib.rs
@@ -66,11 +66,35 @@ fn steamdeck_input_thread(shared: Arc<SteamdeckShared>) {
         for device_info in api.device_list() {
             if device_info.vendor_id() == STEAMDECK_VID_PID.0
                 && device_info.product_id() == STEAMDECK_VID_PID.1
-                && device_info.interface_number() == 0
-                && device_info.usage() == 1
             {
-                device = device_info.open_device(&api);
-                break;
+                println!("Device:");
+                println!("path: {:?}", device_info.path());
+                println!("vendor_id: {}", device_info.vendor_id());
+                println!("product_id: {}", device_info.product_id());
+                println!("serial_number: {:?}", device_info.serial_number());
+                println!("release_number: {}", device_info.release_number());
+                println!(
+                    "manufacturer_string: {:?}",
+                    device_info.manufacturer_string()
+                );
+                println!("product_string: {:?}", device_info.product_string());
+                println!("usage_page: {}", device_info.usage_page());
+                println!("usage: {}", device_info.usage());
+                println!("interface_number: {}", device_info.interface_number());
+                println!("bus_type: {:?}", device_info.bus_type());
+                println!();
+
+                let dev = device_info.open_device(&api);
+
+                if let Ok(dev) = dev {
+                    let mut buf = [0u8; 64];
+                    if let Ok(read) = dev.read_timeout(&mut buf[..], 16) {
+                        if read > 0 {
+                            device = Ok(dev);
+                            break;
+                        }
+                    }
+                }
             }
         }
         device
@@ -82,9 +106,14 @@ fn steamdeck_input_thread(shared: Arc<SteamdeckShared>) {
         println!("OPeN");
         while shared.run.load(Ordering::SeqCst) {
             let mut buf = [0u8; 64];
-            let res = device.read(&mut buf[..]).unwrap();
-            println!("Read: {:?}", &buf[..res]);
+            if let Ok(read) = device.read_timeout(&mut buf[..], 16) {
+                if read > 0 {
+                    println!("Read {read}: {:?}", &buf[..read]);
+                }
+            }
         }
+    } else {
+        println!("No Device");
     }
 
     // Connect to device using its VID and PID
